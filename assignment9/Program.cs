f3cbbd0c962e799cd6d10824609c9a45441ee45d
babujyan <31545427+babujyan@users.noreply.github.com>
2018-05-04 13:33:59 +0400

    first commit

--- assignment9/Program.cs
+++ assignment9/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace assignment9
@@ -10,6 +11,28 @@ namespace assignment9
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            //Console.WriteLine(System.Diagnostics.Process.GetCurrentProcess().Threads.Count);
+            for (int i = 0; i < 1000; i++)
+                lock ((object)i)
+                {
+                    var j = i;
+                    ThreadPool.Run(() =>
+                    {
+
+
+                      {
+                            Thread.Sleep(random.Next(0,1000));
+                            Console.WriteLine(j);
+                      }
+                    //
+
+                    });
+                }
+
+            while (true) ;
+            //Thread.Sleep(5000);
+                //Console.WriteLine(System.Diagnostics.Process.GetCurrentProcess().Threads.Count);
         }
     }
 }
---
+++ assignment9/ThreadPool.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace assignment9
+{
+    static class ThreadPool
+    {
+        private static object notConsumer;
+
+        private static int maxThreadsCount;
+
+        private static int workingThreadsCount;
+
+        private static Queue<Action> actions;
+
+        private static LinkedList<Thread> workingThreads;
+
+        //static Queue<Thread> sleepingThreads;
+
+        static ThreadPool()
+        {
+            notConsumer = 4;
+            maxThreadsCount = 10;
+            workingThreadsCount = 0;
+            actions = new Queue<Action>();
+            workingThreads = new LinkedList<Thread>();
+        }
+
+        public static void Run(Action action)
+        {
+            lock(actions)
+            {
+                actions.Enqueue(action);
+
+            }
+
+
+            if (workingThreads.Count < maxThreadsCount)
+            {
+                
+
+                workingThreads.AddLast(new Thread(() =>
+                {
+ 
+                    while (true)
+                    {
+
+                        Action a = null;
+                        lock (actions)
+                        {
+                            if (actions.Count > 0)
+                            {
+                                a = actions.Dequeue();
+                            }
+                        }
+
+                        if (a != null)
+                        {
+                            a.Invoke();
+                        }
+
+                        lock (actions)
+                        {
+                            if (actions.Count > 0)
+                            {
+                                continue;
+                            }
+                        }
+
+                        lock (notConsumer)
+                            Monitor.Wait(notConsumer);
+
+                    }
+
+                }));
+
+                workingThreads.Last.Value.IsBackground = true;
+                workingThreads.Last.Value.Start();
+            }
+            else
+            {
+                lock (notConsumer)
+                    Monitor.PulseAll(notConsumer);
+            }
+        }
+    }
+}
+
